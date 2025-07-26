dd22b4034de9f97061d7ba69e1994baa8a454ba2
srija2420030062 <2420030062@klh.edu.in>
2025-07-26 15:26:53 +0530

    Add files via upload

---
+++ Bitwise_ope.java
@@ -0,0 +1,26 @@
+package oops_practical;
+import java.util.Scanner;
+
+public class Bitwise_ope {
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("Enter value a: ");
+			int a = sc.nextInt();
+			
+			System.out.println("Enter value b: ");
+			int b = sc.nextInt();
+			
+			int c;
+
+			      c = a & b;  
+			      System.out.println("a & b = " + c);
+
+			      c = a | b;  
+			      System.out.println("a | b = " + c);
+
+			      sc.close();
+		}
+
+	}
+
---
+++ Inc_dec.java
@@ -0,0 +1,32 @@
+package oops_practical;
+import java.util.Scanner;
+
+public class Inc_dec {
+	  public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+
+	    // input values
+	    System.out.print("Enter value for a: ");
+	    int a = sc.nextInt();
+
+	    System.out.print("Enter value for b: ");
+	    int b = sc.nextInt();
+
+	    int result1, result2;
+
+	 
+	    System.out.println("Value of a: " + a);
+
+	   
+	    result1 = ++a;
+	    System.out.println("After increment: " + result1);
+
+	    System.out.println("Value of b: " + b);
+
+	    result2 = --b;
+	    System.out.println("After decrement: " + result2);
+
+	    sc.close();
+	  }
+	}
+
