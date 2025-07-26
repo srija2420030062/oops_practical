3633370ead980f62f68e97bb478bf3779d2eef86
srija2420030062 <2420030062@klh.edu.in>
2025-07-26 15:18:50 +0530

    Add files via upload

---
+++ Artoperators.java
@@ -0,0 +1,33 @@
+
+	package oops_practical;
+	import java.util.Scanner;
+	public class Artoperators {
+
+	  public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+
+
+	    System.out.print("Enter first number (a): ");
+	    int a = sc.nextInt();
+
+	    System.out.print("Enter second number (b): ");
+	    int b = sc.nextInt();
+	    
+	    System.out.println("a + b = " + (a + b));
+
+
+	    System.out.println("a - b = " + (a - b));
+
+
+	    System.out.println("a * b = " + (a * b));
+
+
+	    System.out.println("a / b = " + (a / b));
+
+
+	    System.out.println("a % b = " + (a % b));
+
+	    sc.close();
+	  }
+	}
+
